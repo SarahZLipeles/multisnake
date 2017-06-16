a82b2f4c46aed0f32ff6a86ae6f15420848f3a21
Theo Lipeles <Theo@Lipeles.com>
2017-06-16 15:44:57 -0400

    syntax

--- server/app.js
+++ server/app.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const path = require('path')
-const app = express()
+const express = require("express");
+const path = require("path");
+const app = express();
 
-const socketio = require('socket.io')
-const socket = require('./sockets')
+const socketio = require("socket.io");
+const socket = require("./sockets");
 
-module.exports = app
+module.exports = app;
 
 // Serve static files from ../public
-app.use(express.static(path.join(__dirname, '..', 'public')))
+app.use(express.static(path.join(__dirname, "..", "public")));
 
-const port = 3000
+const port = 3000;
 const server = app.listen(port, function () {
-  console.log('Listening on port', port)
-})
+  console.log("Listening on port", port);
+});
 
-const io = socketio(server)
-socket(io)
+const io = socketio(server);
+socket(io);
