de2aa73a726976046eae0eddc284633eab8ff6ff
Theo Lipeles <Theo@Lipeles.com>
2017-06-16 22:28:24 -0400

    adds player kick on lag

--- server/sockets.js
+++ server/sockets.js
@@ -15,9 +15,28 @@ const socketFunction = io => {
     socket.on("disconnect", () => {
       game.playerLeave(socket.id);
       socket.broadcast.emit("dc", socket.id);
+      game.tick();
+        let timeSinceLastTick = (new Date()) - ticker;
+        setTimeout(() => {
+          ticker = new Date();
+          io.sockets.emit("state", game.state());
+          lagout = true;
+          setTimeout(() => {
+            if (lagout) {
+              socket.disconnect("lag out");
+              // game.playerLeave(socket.id);
+              // socket.broadcast.emit("dc", socket.id);
+            }
+          }, 300);
+        }, (((timeSinceLastTick >= 0) ? (minimumTimeBetweenTicks - timeSinceLastTick) : (0))));
     });
-
+    let lagout = true;
     socket.on("tick", direction => {
+      if (!game.snakes[socket.id]) {
+        game.playerJoin(socket.id);
+        socket.broadcast.emit("connected", socket.id);
+      }
+      lagout = false;
       game.playerMoves[socket.id].move = direction;
       game.playerMoves[socket.id].ready = true;
       if (game.ready()) {
@@ -26,6 +45,14 @@ const socketFunction = io => {
         setTimeout(() => {
           ticker = new Date();
           io.sockets.emit("state", game.state());
+          lagout = true;
+          setTimeout(() => {
+            if (lagout) {
+              socket.disconnect("lag out");
+              // game.playerLeave(socket.id);
+              // socket.broadcast.emit("dc", socket.id);
+            }
+          }, 300);
         }, (((timeSinceLastTick >= 0) ? (minimumTimeBetweenTicks - timeSinceLastTick) : (0))));
 
       }
