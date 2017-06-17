5a4812493ccc17b9a5e1873e3220979b1eea06f1
Theo Lipeles <Theo@Lipeles.com>
2017-06-17 14:42:43 -0400

    player disconnect on lag fix

--- server/sockets.js
+++ server/sockets.js
@@ -6,58 +6,52 @@ const game = new Game();
 const minimumTimeBetweenTicks = 66;
 
 const socketFunction = io => {
-  let ticker = new Date();
-  io.on("connection", socket => {
-    socket.broadcast.emit("connected", socket.id);
+	let ticker = new Date();
+	io.on("connection", socket => {
+		socket.broadcast.emit("connected", socket.id);
 
-    game.playerJoin(socket.id);
+		game.playerJoin(socket.id);
 
-    socket.on("disconnect", () => {
-      game.playerLeave(socket.id);
-      socket.broadcast.emit("dc", socket.id);
-      game.tick();
-        let timeSinceLastTick = (new Date()) - ticker;
-        setTimeout(() => {
-          ticker = new Date();
-          io.sockets.emit("state", game.state());
-          lagout = true;
-          setTimeout(() => {
-            if (lagout) {
-              socket.disconnect("lag out");
+		socket.on("disconnect", () => {
+			game.playerLeave(socket.id);
+			socket.broadcast.emit("dc", socket.id);
+			if (game.ready()) {
+				game.tick();
+				let timeSinceLastTick = (new Date()) - ticker;
+				setTimeout(() => {
+					ticker = new Date();
+					io.sockets.emit("state", game.state());
+				}, (((timeSinceLastTick >= 0) ? (minimumTimeBetweenTicks - timeSinceLastTick) : (0))));
+			}
+		});
+		let lagout = true;
+		socket.on("tick", direction => {
+			if (!game.snakes[socket.id]) {
+				game.playerJoin(socket.id);
+				socket.broadcast.emit("connected", socket.id);
+			}
+			lagout = false;
+			game.playerMoves[socket.id].move = direction;
+			game.playerMoves[socket.id].ready = true;
+			if (game.ready()) {
+				game.tick();
+				let timeSinceLastTick = (new Date()) - ticker;
+				setTimeout(() => {
+					ticker = new Date();
+					io.sockets.emit("state", game.state());
+					lagout = true;
+					setTimeout(() => {
+						if (lagout) {
+							socket.disconnect("lag out");
               // game.playerLeave(socket.id);
               // socket.broadcast.emit("dc", socket.id);
-            }
-          }, 300);
-        }, (((timeSinceLastTick >= 0) ? (minimumTimeBetweenTicks - timeSinceLastTick) : (0))));
-    });
-    let lagout = true;
-    socket.on("tick", direction => {
-      if (!game.snakes[socket.id]) {
-        game.playerJoin(socket.id);
-        socket.broadcast.emit("connected", socket.id);
-      }
-      lagout = false;
-      game.playerMoves[socket.id].move = direction;
-      game.playerMoves[socket.id].ready = true;
-      if (game.ready()) {
-        game.tick();
-        let timeSinceLastTick = (new Date()) - ticker;
-        setTimeout(() => {
-          ticker = new Date();
-          io.sockets.emit("state", game.state());
-          lagout = true;
-          setTimeout(() => {
-            if (lagout) {
-              socket.disconnect("lag out");
-              // game.playerLeave(socket.id);
-              // socket.broadcast.emit("dc", socket.id);
-            }
-          }, 300);
-        }, (((timeSinceLastTick >= 0) ? (minimumTimeBetweenTicks - timeSinceLastTick) : (0))));
+						}
+					}, 300);
+				}, (((timeSinceLastTick >= 0) ? (minimumTimeBetweenTicks - timeSinceLastTick) : (0))));
 
-      }
-    });
-  });
+			}
+		});
+	});
 };
 
 module.exports = socketFunction;
