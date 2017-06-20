c4eac82d2c93317da3eef974e1ac293dc715be90
JonathanLiu25 <JonathanLiu25@gmail.com>
2017-06-20 12:23:28 -0400

    adds die method to Food

--- server/Models/Food.js
+++ server/Models/Food.js
@@ -5,6 +5,17 @@ module.exports = class Food extends Entity {
 		super(Math.floor(Math.random() * (range + range) - range),
 			Math.floor(Math.random() * (range + range) - range),
 			Math.floor(Math.random() * (range + range) - range));
+		this.range = range;
 		this.value = value;
+	}
+
+	randomPos() {
+		return Math.floor(Math.random() * (this.range + this.range) - this.range);
+	}
+
+	die() {
+		this.x = this.randomPos();
+		this.y = this.randomPos();
+		this.z = this.randomPos();
 	}
 };
